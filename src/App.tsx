b28cceb27e46c005f332ac02a1d657e1b1276fc0
sb0m <susann.bombor@carasent.com>
2024-01-24 14:06:52 +0100

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from "react";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -15,14 +13,6 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
