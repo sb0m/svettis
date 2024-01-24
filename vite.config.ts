88c10ee97532ede6737a34a91d2001af2d6e4579
sb0m <susann.bombor@carasent.com>
2024-01-24 13:09:18 +0100

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
+// import { VitePWA } from "vite-plugin-pwa";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+//   base: "/Svettis/",
+// });
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [react()],
   base: "/Svettis/",
 });
