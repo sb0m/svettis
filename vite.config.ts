9ab63c0d5fd122851712820511ce1cde4b777101
sb0m <susann.bombor@carasent.com>
2024-02-15 10:02:05 +0100

    Test vibrate

--- vite.config.ts
+++ vite.config.ts
@@ -52,7 +52,7 @@ const manifestForPlugIn = {
       //   purpose: "any",
       // },
       {
-        src: "/svettis/icons/icon-512x512.png",
+        src: "/svettis/icons/icon-384x384.png",
         sizes:
           "72x72 96x96 128x128 144x144 152x152 192x192 256x256 384x384 512x512",
         type: "image/png",
